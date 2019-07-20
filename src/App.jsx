f9ac3c035d72c7389d47f519dfc16d0259ba9097
Brandon Fryslie <530235+brandon-fryslie@users.noreply.github.com>
2019-07-20 13:50:21 -0600

    Fix bug where clicking 'Reset Scene' was broken for all non-video clip types

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader/root';
 import MainContent from './components/main-content';
-import UITweaks from './UITweaks';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
     return <MainContent />;
   }
